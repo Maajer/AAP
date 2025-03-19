1a4e4851ffae12bb5ca2d5244489127477e3417c
Maajer <martin.majer@infis.cz>
2025-03-19 13:25:12 +0100

    Add files via upload

---
+++ úkol.py
@@ -0,0 +1,24 @@
+#úkol 1
+jméno = input("Zadej svoje jméno")
+věk = input("Zadej svůj věk")
+print (f"Ahoj, jmenuji se {jméno} a je mi {věk} let")
+
+#úkol 2
+číslo1 = float(input("Zadej první desetinné číslo: "))
+číslo2 = float(input("Zadej druhé desetinné číslo: "))
+
+součet = cislo1 + cislo2
+rozdíl = cislo1 - cislo2
+součin = cislo1 * cislo2
+
+if cisl 2 != 0:
+    podíl = cislo1 / cislo2
+else:
+    podíl = none
+
+print(f"Součet: {součet:.2f}")
+print(f"Rozdíl: {rozdíl:.2f}")
+print(f"Součin: {součin:.2f}")
+if podíl is not None:
+    print("Podíl nelze vypočítat (dělení nulou)")
+#uprava
